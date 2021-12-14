4de8b7ee7911b6ccf434c233b8934db5bed1b1f8
MysticVagabond <63424123+MysticVagabond@users.noreply.github.com>
2021-12-14 19:27:59 +0000

    Started client app
    Added ts project features
    Added recoil state management
    Added apollo client and graphql
    
    Confirmed that project in current state builds, runs and tests

---
+++ RecoilCryptoDashboard/cryptodashboardv2/src/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: 'TBD', // localhost 3001
+  cache: new InMemoryCache()
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>,
+  document.getElementById('root')
+);
