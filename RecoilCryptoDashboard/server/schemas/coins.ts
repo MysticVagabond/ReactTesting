03d5a0783a8ceb7c775a56176cfea91d66ccf03a
MysticVagabond <63424123+MysticVagabond@users.noreply.github.com>
2021-12-15 16:26:48 +0000

    Update coins.ts
    Added in hard coded prices at time of writing and todo list

--- RecoilCryptoDashboard/server/schemas/coins.ts
+++ RecoilCryptoDashboard/server/schemas/coins.ts
@@ -1,58 +1,62 @@
+// TODO: 
+// Change this to a MongoDB to persist changes
+// Grab the current prices from coingecko https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Ckava%2Cpolkadot%2Cbinance-eth%2Ccrypto-com-chain%2Cstellar%2Cpancakeswap-token&vs_currencies=usd
+// Grab the current exchange rate from USD to GBP from http://free.currencyconverterapi.com/api/v5/convert?q=USD_GBP&compact=y&apiKey=[APIKEY]
+
 export const ethereum = {
     name: 'Ethereum',
     coinGeckoName: 'ethereum',
     symbol: 'ETH',
-    value: 0
+    value: 3686.79
 }
   
 export const cryptoDotCom = {
     name: 'Crypto.com Coin',
     coinGeckoName: 'crypto-com-chain',
     symbol: 'CRO',
-    value: 0
+    value: 0.518116
 }
 
 export const shibaInu = {
     name: 'Shiba Inu',
     coinGeckoName: 'shiba-inu',
     symbol: 'SHIB',
-    value: 0
+    value: 0.00003242
 }
 
-export const binanceEthereum
-= {
+export const binanceEthereum = {
     name: 'Binance Ethereum',
     coinGeckoName: 'binance-eth-staking',
     symbol: 'BETH',
-    value: 0
+    value: 3421.66
 }
 
 export const cake = {
     name: 'Pancake Swap',
     coinGeckoName: 'pancakeswap',
     symbol: 'CAKE',
-    value: 0
+    value: 12.08
 }
 
 export const loopring = {
     name: 'Loopring',
     coinGeckoName: 'loopring',
     symbol: 'LRC',
-    value: 0
+    value: 2.03
 }
 
 export const polkadot = {
     name: 'Polkadot',
     coinGeckoName: 'polkadot',
     symbol: 'DOT',
-    value: 0
+    value: 24.48
 }
 
 export const kava = {
     name: 'Kava',
     coinGeckoName: 'kava',
     symbol: 'KAVA',
-    value: 0
+    value: 3.24
 }
 
 export const coinList = [ethereum, cryptoDotCom, shibaInu, binanceEthereum, loopring, cake, polkadot, kava];
